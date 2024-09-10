fbf30323bf45cc57fcb9faae5e039cdd4987719d
murarirc <raphaelmcaceres@gmail.com>
2024-09-10 11:56:05 -0300

    Aula0910 - footer e navbar

---
+++ nomadelife-front/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <>
+        <footer classname={styles.footer}>
+            <h3>Plataforma para desenvolvedores que tem o espírito lívre</h3>
+            <p>Nomade Developers</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
--- nomadelife-front/src/components/NavBar.jsx
+++ nomadelife-front/src/components/NavBar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import styles from './NavBar.module.css'
 
 export const NavBar = () => {
   return (
     <>
+    <nav classname={styles.navbar}>
+        <div classname={styles.brand}>
+            Nomade<span>DEV</span>
+        </div>
+        <ul classname={styles.links_list}>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Log In</a>
+            </li>
+            <li>
+                <a href="#">Register</a>
+            </li>
+            <li>
+                <a href="#">New Post</a>
+            </li>
+            <li>
+                <a href="#">Dash Board</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Edit</a>
+            </li>
+        </ul>
+    </nav>
     </>
   )
 }
