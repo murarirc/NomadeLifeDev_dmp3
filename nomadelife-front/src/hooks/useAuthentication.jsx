e3cbcbcb04c92ca31b3162bcf75b5cf42b0689ef
murarirc <raphaelmcaceres@gmail.com>
2024-09-24 11:40:59 -0300

    Aula0924 - Hook de authentication

--- nomadelife-front/src/hooks/useAuthentication.jsx
+++ nomadelife-front/src/hooks/useAuthentication.jsx
@@ -20,4 +20,97 @@ export const useAuthentication = () => {
             return
         }
     }
+    async function createUser(data){
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(null)
+
+        try{
+            const { user } = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+            await updateProfile(
+                user, {
+                    displayName: data.displayName,
+
+                }
+            )
+            setLoading(false)
+
+            return user
+        }
+        catch(error){
+            console.error(error.message)
+            console.table(typeof error.message)
+
+            let systemErrorMessage
+
+            if(error.message.include('password')){
+                systemErrorMessage = "A senha precisa conter ao menos 6 caracteres"
+            }else if(error.message.include('email-already')){
+                systemErrorMessage = "E-mail já cadastrado em nosso sistema"
+            }else{
+                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde"
+            }
+
+            setLoading(false)
+            setError(systemErrorMessage)
+        }
+    }
+
+    const login = async (data) =>{
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(null)
+
+        try{
+            await signInWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+            setLoading(false)
+        }
+        catch(error){
+            console.error(error.message)
+            console.table(typeof error.message)
+
+            let systemErrorMessage
+
+            if(error.message.include('invalid-login-credentials')){
+                systemErrorMessage = "Este usuário não tem registro em nossos sistemas"
+            }else if(error.message.include('wrong-password')){
+                systemErrorMessage = "Existe algum erro em suas credenciais de login"
+            }else{
+                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde"
+            }
+
+            setLoading(false)
+            setError(systemErrorMessage)
+        }
+    }
+
+    const logout = ()=>{
+        checkIfIsCancelled()
+        signOut(auth)
+    }
+
+    useEffect(
+        () =>{
+            return () => setCancelled(true)
+        }, []
+    )
+    return{
+        auth,
+        createUser,
+        error,
+        loading,
+        logout,
+        login
+    }
 }
